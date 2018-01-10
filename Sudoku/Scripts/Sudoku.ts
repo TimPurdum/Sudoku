67ddc31fceb74fa8ac5737522fd6d7919a8bb437
Tim Purdum <tim@cedarrivertech.com>
2018-01-10 17:36:41 -0600

    Improved layouts Added keyboard shortcuts - space (toggles pencil) - arrow key navigation - numbers

--- Sudoku/Scripts/Sudoku.ts
+++ Sudoku/Scripts/Sudoku.ts
@@ -1,4 +1,6 @@
 ﻿var pencilMode = false;
+var row = 0;
+var column = 0;
 
 function togglePencilMark(box: HTMLButtonElement, index: number) {
     if (box.innerHTML === index.toString()) {
@@ -14,16 +16,14 @@ function togglePencilMode(button: HTMLButtonElement) {
     let table: HTMLTableElement;
     if (pencilMode) {
         //turn off pencil
-        button.classList.remove('btn-danger');
-        button.classList.add('btn-primary');
+        button.style.backgroundColor = 'gray';
         for (table of pencilTables) {
             if (table.classList.contains('pencil')) {
                 table.style.zIndex = '1';
             }
         }
     } else {
-        button.classList.remove('btn-primary');
-        button.classList.add('btn-danger');
+        button.style.backgroundColor = 'red';
         for (table of pencilTables) {
             var guess = table.parentElement.lastElementChild as HTMLInputElement;
             if (table.classList.contains('pencil') && !guess.value) {
@@ -39,6 +39,7 @@ function saveGuess(row: number, column: number, guess: HTMLInputElement) {
         eraseGuess(guess);
     } else {
         guess.style.backgroundColor = 'white';
+        guess.style.color = 'black';
     }
 }
 
@@ -47,4 +48,63 @@ function eraseGuess(guess: HTMLInputElement) {
     if (pencilMode) {
         pencilTable.style.zIndex = '3';
     }
+    guess.style.backgroundColor = 'transparent';
+    guess.style.color = 'transparent';
 }
+
+function setSquare(r: number, c: number) {
+    row = r;
+    column = c;
+    const square = document.getElementById(`square-${row}-${column}`) as HTMLInputElement;
+    square.style.borderWidth = '3px';
+    if (!square.value) {
+        square.style.color = 'transparent';
+    }
+}
+
+document.addEventListener('keydown', (e) => {    
+    if (e.keyCode > 36 && e.keyCode < 41) {
+        const oldSquare = document.getElementById(`square-${row}-${column}`);
+        oldSquare.style.borderWidth = '1px';
+        
+        switch (e.keyCode) {
+            case 40:
+                if (row < 8) {
+                    row++;
+                }
+                break;
+            case 38:
+                if (row > 0) {
+                    row--;
+                }
+                break;
+            case 37:
+                if (column > 0) {
+                    column--;
+                }
+                break;
+            case 39:
+                if (column < 8) {
+                    column++;
+                }
+                break;
+        }
+        const newSquare = document.getElementById(`square-${row}-${column}`);
+        newSquare.style.borderWidth = '3px';
+        newSquare.focus();
+        
+        e.preventDefault();
+    } else if (e.keyCode > 48 && e.keyCode < 58) {
+        if (pencilMode) {
+            e.preventDefault();
+            const index = e.keyCode - 48;
+            const pencil = document.getElementById(`p-${row}-${column}-${index}`) as HTMLButtonElement;
+            togglePencilMark(pencil, index);
+        }
+    } else if (e.keyCode === 32) {
+        togglePencilMode(document.getElementById('pencil-toggle-button') as HTMLButtonElement);
+        e.preventDefault();
+    } else if (e.keyCode === 8) {
+        (document.getElementById(`square-${row}-${column}`) as HTMLInputElement).value = '';
+    }
+});
--- Sudoku/wwwroot/js/Sudoku.js
+++ Sudoku/wwwroot/js/Sudoku.js
@@ -1,4 +1,6 @@
 var pencilMode = false;
+var row = 0;
+var column = 0;
 function togglePencilMark(box, index) {
     if (box.innerHTML === index.toString()) {
         box.innerHTML = '_';
@@ -12,8 +14,7 @@ function togglePencilMode(button) {
     let table;
     if (pencilMode) {
         //turn off pencil
-        button.classList.remove('btn-danger');
-        button.classList.add('btn-primary');
+        button.style.backgroundColor = 'gray';
         for (table of pencilTables) {
             if (table.classList.contains('pencil')) {
                 table.style.zIndex = '1';
@@ -21,8 +22,7 @@ function togglePencilMode(button) {
         }
     }
     else {
-        button.classList.remove('btn-primary');
-        button.classList.add('btn-danger');
+        button.style.backgroundColor = 'red';
         for (table of pencilTables) {
             var guess = table.parentElement.lastElementChild;
             if (table.classList.contains('pencil') && !guess.value) {
@@ -38,6 +38,7 @@ function saveGuess(row, column, guess) {
     }
     else {
         guess.style.backgroundColor = 'white';
+        guess.style.color = 'black';
     }
 }
 function eraseGuess(guess) {
@@ -45,4 +46,62 @@ function eraseGuess(guess) {
     if (pencilMode) {
         pencilTable.style.zIndex = '3';
     }
+    guess.style.backgroundColor = 'transparent';
+    guess.style.color = 'transparent';
 }
+function setSquare(r, c) {
+    row = r;
+    column = c;
+    const square = document.getElementById(`square-${row}-${column}`);
+    square.style.borderWidth = '3px';
+    if (!square.value) {
+        square.style.color = 'transparent';
+    }
+}
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode > 36 && e.keyCode < 41) {
+        const oldSquare = document.getElementById(`square-${row}-${column}`);
+        oldSquare.style.borderWidth = '1px';
+        switch (e.keyCode) {
+            case 40:
+                if (row < 8) {
+                    row++;
+                }
+                break;
+            case 38:
+                if (row > 0) {
+                    row--;
+                }
+                break;
+            case 37:
+                if (column > 0) {
+                    column--;
+                }
+                break;
+            case 39:
+                if (column < 8) {
+                    column++;
+                }
+                break;
+        }
+        const newSquare = document.getElementById(`square-${row}-${column}`);
+        newSquare.style.borderWidth = '3px';
+        newSquare.focus();
+        e.preventDefault();
+    }
+    else if (e.keyCode > 48 && e.keyCode < 58) {
+        if (pencilMode) {
+            e.preventDefault();
+            const index = e.keyCode - 48;
+            const pencil = document.getElementById(`p-${row}-${column}-${index}`);
+            togglePencilMark(pencil, index);
+        }
+    }
+    else if (e.keyCode === 32) {
+        togglePencilMode(document.getElementById('pencil-toggle-button'));
+        e.preventDefault();
+    }
+    else if (e.keyCode === 8) {
+        document.getElementById(`square-${row}-${column}`).value = '';
+    }
+});
